cbc146e99c5d2ca1008933475345612de8924e16
Fher García <fernanAnt.0711@hotmail.com>
2024-10-26 22:07:29 -0600

    second commit

---
+++ front-end/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to Ninja Deal</h1>
+            <p>This is the home page of Ninja Deal. Explore our deals and offers!</p>
+        </div>
+    );
+};
+
+export default Home;
