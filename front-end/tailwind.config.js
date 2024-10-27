f05486d146cdb9bffa9eed75b43e2b1a095e4119
Fher García <fernanAnt.0711@hotmail.com>
2024-10-26 22:43:12 -0600

    footer and header change

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,10 +1,17 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
+
+module.exports = withMT(module.exports);
