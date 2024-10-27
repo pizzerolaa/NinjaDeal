b962638dd242c3853eac0d9d4ecbb2c9a0fea411
Fher García <fernanAnt.0711@hotmail.com>
2024-10-26 22:57:05 -0600

    add simple header

--- front-end/src/components/layout/Header.tsx
+++ front-end/src/components/layout/Header.tsx
@@ -6,6 +6,13 @@ const Header: React.FC = () => {
         <header className="bg-blue-500 text-white p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <h1 className="text-2xl font-bold">Ninja Deal</h1>
+                <nav>
+                    <ul className="flex space-x-4">
+                        <li><a href="#" className="hover:text-gray-300">Home</a></li>
+                        <li><a href="#" className="hover:text-gray-300">About</a></li>
+                        <li><a href="#" className="hover:text-gray-300">Contact</a></li>
+                    </ul>
+                </nav>
             </div>
         </header>
     );
